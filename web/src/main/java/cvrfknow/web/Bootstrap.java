f649b9e5bc7a280d09ab0cedb3b91a0a9aa0f9b0
Sean Monaghan <smonagan000@gmail.com>
2014-03-17 15:53:24 -0400

    adding CvrfdocLoader and wiring up temp Bootstrap for web

--- web/src/main/java/cvrfknow/web/Bootstrap.java
+++ web/src/main/java/cvrfknow/web/Bootstrap.java
@@ -1,30 +1,44 @@
 package cvrfknow.web;
 
-import gem.domain.Attribute;
+import cvrfknow.model.CvrfdocLoader;
+import cvrfknow.model.EntityGraphAssembler;
 import gem.domain.Entity;
 import gem.service.EntityService;
 import gem.service.exception.EntityRejectedException;
+import org.icasi.cvrf.schema.cvrf.Cvrfdoc;
+import org.icasi.cvrf.schema.vuln.Vulnerability;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.UUID;
+import java.io.InputStream;
+import java.util.List;
 
-@Component
+
 public class Bootstrap {
 
     @Autowired(required = true)
     private EntityService entityService;
 
-    public Bootstrap() {
+    private List<String> cvrfSeed = null;
+
+    public Bootstrap(List<String> cvrfSeed) {
+        this.cvrfSeed = cvrfSeed;
     }
 
     @PostConstruct
-    public void init() throws EntityRejectedException {
-        System.out.println("cvrfknow.web.Bootstrap >>> init");
-        Entity e = new Entity(UUID.randomUUID().toString(), "TEST");
-        e.addAttribute(new Attribute("name", "Sean", "U"));
-        entityService.saveObject(e, false);
+    public void init() throws Exception {
+        for (String seed : cvrfSeed) {
+            InputStream is = this.getClass().getResourceAsStream(seed);
+            if (is != null) {
+                Cvrfdoc doc = CvrfdocLoader.loadCvrfdoc(is);
+                for (Vulnerability v : doc.getVulnerability()) {
+                    saveEntities(EntityGraphAssembler.assemble(v));
+                }
+            }
+        }
+    }
 
+    private void saveEntities(List<Entity> entities) throws EntityRejectedException {
+        entityService.saveObjects(entities.iterator(), false);
     }
 }
